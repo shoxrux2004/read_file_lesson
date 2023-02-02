bbf6e27d885fbb5758843c2c5206ef40721e6fa7
ikromyusuf <ikromyusupovdev@gmail.com>
2023-02-02 19:07:38 +0500

    done

--- read_file.py
+++ read_file.py
@@ -1,21 +1,15 @@
 #Rread file and convert to list
 def read_file(filename: str) -> list:
     """
-    Reads a file and returns a list of int digits.
+    Reads a file and returns a list of integers.
     
     Args:
         filename (str): The name of the file to read.
     Returns:
-        data (list): A list of list of int digits from the file.
+        data (list): A list of list of integers from the file.
     """
     # Open the file
-    f = open(filename).read()
-    f = f.split(",")
-    list_of_integers = []
-    for i in f:
-        list_of_integers.append(int(i))
     # Read the file
-    return list_of_integers
+    return 0
 
 #Print list from file
-print(read_file("data.txt"))
