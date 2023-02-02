4089701f115b0fc21dac4ffee1e0ed64ed6b43be
ikromyusuf <ikromyusupovdev@gmail.com>
2023-02-02 18:40:22 +0500

    instruction changed

--- read_file.py
+++ read_file.py
@@ -1,13 +1,12 @@
 #Rread file and convert to list
 def read_file(filename: str) -> list:
     """
-    Reads a file and returns a list of lines.
+    Reads a file and returns a list of int digits.
     
     Args:
         filename (str): The name of the file to read.
-    
     Returns:
-        data (list): A list of lines from the file.
+        data (list): A list of list of int digits from the file.
     """
     # Open the file
     # Read the file
