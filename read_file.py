be377485223b55b286d77fc25967e5e6adab9890
ikromyusuf <ikromyusupov09@gmail.com>
2023-04-17 15:44:38 +0500

    instruction changed

--- read_file.py
+++ read_file.py
@@ -6,7 +6,7 @@ def read_file(filename: str) -> list[int]:
     Args:
         filename (str): The name of the file to read.
     Returns:
-        data (list): A list of list of integers from the file.
+        data (list): A list of integers from the file.
     """
     # Open the file
     # Read the file
