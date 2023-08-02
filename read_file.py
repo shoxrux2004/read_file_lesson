d940ca592ada84d646f362d2c9c73076aa5adb67
shoxrux <shoxruxnorqoziyev2004@gmail.com>
2023-08-02 17:08:47 +0500

    filedagi strni intga utkazish

--- read_file.py
+++ read_file.py
@@ -2,14 +2,20 @@
 def read_file(filename: str) -> list[int]:
     """
     Reads a file and returns a list of integers.
-
     Args:
         filename (str): The name of the file to read.
     Returns:
         data (list): A list of integers from the file.
-    """
-    # Open the file
-    # Read the file
-    return 0 
-
-#Print list from file
+#     """
+#     # Open the file
+#     # Read the file
+#     return 0 
+# #Print list from file
+    list1=[]
+    f=open(filename)
+    a=f.read()
+    t=a.split(',')
+    for i in t:
+        list1.append(int(i))
+    return list1
+print(read_file("data.txt"))
